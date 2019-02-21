16c42824b348eabeae37fb513c61739fc6ee765d
Mark Czotter <mczotter@b2international.com>
2019-02-21 17:53:58 +0100

    Initial version of use-java-8 and use-java-8 ps scripts

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # use-java
-Windows 10 PowerShell Scripts to easily switch between Java 8 and 11 (via https://github.com/shyiko/jabba)
+
+Windows 10 PowerShell Scripts to easily switch between Java 8 and 11 (via https://github.com/shyiko/jabba) and save time :coffee:
+
+# Install
+
+1. Install `jabba` cli tool from https://github.com/shyiko/jabba
+2. Run `use-java-8.ps1` or `use-java-11.ps1` script depending on your use case
+
+The script will install and use a hardcoded version of Java 8/Java 11 respectively via `jabba` and will also change the `JAVA_HOME` and `PATH` environment variables so tools that depend on those variables will work properly. :sunglasses:
---
+++ use-java-11.ps1
@@ -0,0 +1,17 @@
+If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+{
+  # Relaunch as an elevated process:
+  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs -NoNewWindow
+  exit
+}
+
+$jdk="openjdk@1.11.0"
+jabba install $jdk
+jabba use $jdk
+
+$envRegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', $true)
+$envPath=$envRegKey.GetValue('Path', $null, "DoNotExpandEnvironmentNames").replace('$JAVA_HOME\bin;', '')
+[Environment]::SetEnvironmentVariable('JAVA_HOME', "$(jabba which $(jabba current))", 'Machine')
+[Environment]::SetEnvironmentVariable('PATH', "$(jabba which $(jabba current))/bin;$envPath", 'Machine')
+
+echo "Switched to $jdk"
