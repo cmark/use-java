a29838547c105091e545a10352978afa25d8c505
Mark Czotter <mczotter@b2international.com>
2019-03-08 14:17:03 +0100

    Remove -NoNewWindow causing issues when trying to elevate to admin

--- use-java-11.ps1
+++ use-java-11.ps1
@@ -1,17 +1,25 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
-{
-  # Relaunch as an elevated process:
-  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs -NoNewWindow
-  exit
+Try 
+{ 
+  If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+  {
+    # Relaunch as an elevated process:
+	Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+    exit
+  }
+
+  $jdk="openjdk@1.11.0"
+  jabba install $jdk
+  jabba use $jdk
+
+  $envRegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', $true)
+  $envPath=$envRegKey.GetValue('Path', $null, "DoNotExpandEnvironmentNames").replace('$JAVA_HOME\bin;', '')
+  [Environment]::SetEnvironmentVariable('JAVA_HOME', "$(jabba which $(jabba current))", 'Machine')
+  [Environment]::SetEnvironmentVariable('PATH', "$(jabba which $(jabba current))/bin;$envPath", 'Machine')
+  echo "Switched to $jdk"
 }
-
-$jdk="openjdk@1.11.0"
-jabba install $jdk
-jabba use $jdk
-
-$envRegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', $true)
-$envPath=$envRegKey.GetValue('Path', $null, "DoNotExpandEnvironmentNames").replace('$JAVA_HOME\bin;', '')
-[Environment]::SetEnvironmentVariable('JAVA_HOME', "$(jabba which $(jabba current))", 'Machine')
-[Environment]::SetEnvironmentVariable('PATH', "$(jabba which $(jabba current))/bin;$envPath", 'Machine')
-
-echo "Switched to $jdk"
+Catch 
+{
+  $ErrorMessage = $_.Exception.Message
+  Write-Error -Message "$ErrorMessage"
+  read-host 'Press Enter to continue...' 
+}
--- use-java-8.ps1
+++ use-java-8.ps1
@@ -1,17 +1,26 @@
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+Try
 {
-  # Relaunch as an elevated process:
-  Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs -NoNewWindow
-  exit
+  If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
+  {
+    # Relaunch as an elevated process:
+    Start-Process powershell.exe "-File",('"{0}"' -f $MyInvocation.MyCommand.Path) -Verb RunAs
+    exit
+  }
+
+  $jdk="adopt@1.8.202-08"
+  jabba install $jdk
+  jabba use $jdk
+
+  $envRegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', $true)
+  $envPath=$envRegKey.GetValue('Path', $null, "DoNotExpandEnvironmentNames").replace('$JAVA_HOME\bin;', '')
+  [Environment]::SetEnvironmentVariable('JAVA_HOME', "$(jabba which $(jabba current))", 'Machine')
+  [Environment]::SetEnvironmentVariable('PATH', "$(jabba which $(jabba current))/bin;$envPath", 'Machine')
+
+  echo "Switched to $jdk"
 }
-
-$jdk="adopt@1.8.202-08"
-jabba install $jdk
-jabba use $jdk
-
-$envRegKey = [Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', $true)
-$envPath=$envRegKey.GetValue('Path', $null, "DoNotExpandEnvironmentNames").replace('$JAVA_HOME\bin;', '')
-[Environment]::SetEnvironmentVariable('JAVA_HOME', "$(jabba which $(jabba current))", 'Machine')
-[Environment]::SetEnvironmentVariable('PATH', "$(jabba which $(jabba current))/bin;$envPath", 'Machine')
-
-echo "Switched to $jdk"
+Catch 
+{
+  $ErrorMessage = $_.Exception.Message
+  Write-Error -Message "$ErrorMessage"
+  read-host 'Press Enter to continue...' 
+}
